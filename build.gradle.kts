21d4cc19f1a5a97a38e56c18afa63ebc335ad769
Saket Narayan <saket@saket.me>
2026-03-24 09:20:14 -0700

    Set up IntelliJ plugin project with Gradle
    Configures the project skeleton for an IntelliJ platform plugin
    targeting 2024.1+ using the IntelliJ Platform Gradle Plugin 2.x.

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+  id("java")
+  id("org.jetbrains.kotlin.jvm") version "2.0.21"
+  id("org.jetbrains.intellij.platform") version "2.3.0"
+}
+
+group = providers.gradleProperty("pluginGroup").get()
+version = providers.gradleProperty("pluginVersion").get()
+
+kotlin {
+  jvmToolchain(17)
+}
+
+repositories {
+  mavenCentral()
+  intellijPlatform {
+    defaultRepositories()
+  }
+}
+
+dependencies {
+  intellijPlatform {
+    intellijIdeaCommunity(providers.gradleProperty("platformVersion").get())
+    bundledPlugin("com.intellij.java")
+    testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+  }
+  testImplementation("junit:junit:4.13.2")
+  testImplementation("com.willowtreeapps.assertk:assertk:0.28.1")
+}
+
+tasks.test {
+  testLogging {
+    showStandardStreams = true
+  }
+}
+
+intellijPlatform {
+  pluginConfiguration {
+    name = providers.gradleProperty("pluginName")
+    version = providers.gradleProperty("pluginVersion")
+
+    ideaVersion {
+      sinceBuild = providers.gradleProperty("pluginSinceBuild")
+    }
+  }
+}
